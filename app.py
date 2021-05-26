f1970a2ccd95a13480c147f4bc2f8d0c958e9022
Moshood Sikiru <sikirumoshood@gmail.com>
2021-05-26 16:47:19 +0100

    Flask crud app

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://moshoodsikiru:root@localhost:5432/todo_app'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        taskContent = request.form['content']
+        newTask = Todo(content=taskContent)
+
+        try:
+            db.session.add(newTask)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'The was issue adding task'
+    else:
+        tasks = Todo.query.order_by(Todo.created_at).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def deleteTask(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Failed to delete task'
+
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def updateTask(id):
+    task_to_update = Todo.query.get_or_404(id)
+    if request.method == 'GET':
+        return render_template('update.html', task=task_to_update)
+    else:
+        task_to_update.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Failed to update task'
+
+
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html', tier1Price=3000, tier2Price=5000)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
